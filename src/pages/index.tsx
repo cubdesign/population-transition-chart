db8c75865e43780a41b41deb3a7bc81a530aa319
cubdesign <takeo@cubdesign.com>
2022-11-18 18:39:21 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>population-transition-chart</title>
+        <meta name="description" content="population-transition-chart" />
+      </Head>
+
+      <main>
+        <h1>population-transition-chart</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
